a7054a425fc40d8e8a2fa3f4531dcc3615441c39
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2018-06-14 10:50:39 +0300

    More configuration

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const BootBot = require('bootbot'); //https://github.com/Charca/bootbot
-const express = require('express');
+'use strict';
+const BootBot = require('bootbot'); //  https://github.com/Charca/bootbot
 const options = require('./options');
 const {Wit, log} = require('node-wit');
 
 const bot = new BootBot({
-    accessToken: options.accessToken,
-    verifyToken: options.verifyToken,
-    appSecret: options.appSecret
+  accessToken: options.accessToken,
+  verifyToken: options.verifyToken,
+  appSecret: options.appSecret,
 });
 
 const wit = new Wit({
-    accessToken: options.witToken,
-    logger: new log.Logger(log.DEBUG) // optional
+  accessToken: options.witToken,
+  logger: new log.Logger(log.DEBUG), // optional
 });
 
 
 // Users
 bot.on('message', (payload, chat) => {
-    // Receive message from payload.sender.id
-    // wit.message(payload.message.text, JSON.stringify(payload.message.nlp))
-    //     .then((data) => {
-    //         // write an answer to the user
-    //         operationSelector(chat, data);
-    //     })
-    //     .catch(console.error);
-    chat.say("hello");
+  // Receive message from payload.sender.id
+  wit.message(payload.message.text, JSON.stringify(payload.message.nlp))
+    .then((data) => {
+      // write an answer to the user
+    })
+    .catch(console.error);
+
+  chat.say('hello');
 });
 
 
---
+++ options.js
@@ -0,0 +1,12 @@
+'use strict';
+/* eslint-disable max-len */
+const accessToken = 'aaaa';
+/* eslint-enable max-len */
+const verifyToken = 'aaa';
+const appSecret = 'aaa';
+const witToken = 'aaa';
+
+exports.accessToken = accessToken;
+exports.verifyToken = verifyToken;
+exports.appSecret = appSecret;
+exports.witToken = witToken;
